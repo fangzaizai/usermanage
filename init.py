94c144ef0147a8539f5d296e559c7ce83f8be8bf
fangzaizai <anmeifang111@sina,com>
2016-10-11 10:10:02 +0800

    add cookie but json····
    need to solve return value of json

--- init.py
+++ init.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
 
 import sqlite3
-from flask import Flask,g,session,request,redirect,url_for,abort,render_template,flash,jsonify
+from flask import Flask,g,session,request,redirect,url_for,json,abort
+from flask import render_template,flash,jsonify,make_response,Response,request
 from contextlib import closing
 from datetime import timedelta
+import json
 
 error=''
+global USRTEMP
+USRTEMP=''
 
 app=Flask(__name__)
 app.config.from_pyfile('yourconfig.cfg', silent=True)
@@ -38,25 +42,33 @@ def login():
 		usrexist=g.cursor.execute('select * from user where uaername=(?)', [usrtologin]).fetchall()
 		pswexist=g.cursor.execute('select * from user where uaername=(?) and password=(?)', [usrtologin,pswtologin]).fetchall()
 		if ((usrtologin == app.config['USERNAME']) & (pswtologin == app.config['PASSWORD'])):
-			session['logined']=True
+			#session
+			USRTEMP=usrtologin
+			session['logined']=usrtologin
 			return redirect(url_for('user_list'))
 		elif not (usrexist):
 			error='username not exist'
+			return jsonify(result='2')
 		elif not (pswexist):
 			error='wrong password!'
+			return jsonify(result='3')
 		else:
+			#error='en'
 			session.permanent = True
 			app.permanent_session_lifetime = timedelta(minutes=10)
-			session['logined']=True
-			global usrtemp
-			usrtemp=usrtologin
+			session['logined']=usrtologin
+			USRTEMP=usrtologin
+			res={'result':'1'}
+			return redirect(url_for('user_list'))
+			return Response(json.dumps(res), mimetype='application/json')
+			#return jsonify(result='1')
 			return redirect(url_for('user_list'))
 	return render_template('login.html',error=error)
 
 @app.route('/logout',methods=['GET','POST'])
 def logout():
 	session.pop('logined',None)
-	flash('log out successfgully!','info') 
+	flash('log out successfgully!','info')
 	return redirect(url_for('user_list'))
 
 @app.route('/',methods=['GET','POST'])
@@ -79,9 +91,9 @@ def add_user():
 		if not (usrexist):
 			g.cursor.execute('insert into user(uaername,password,level) values (?,?,?)', [usr,psw,ulevel])
 			g.db.commit()
-			return jsonify(result='add successfully！')
+			return jsonify(result='add successfully')
 		else:
-			return jsonify(result='usr already exist,change it')			
+			return jsonify(result='usr already exist,change it')		
 		#return redirect(url_for('user_list'))
 	except Exception,e:
 		return jsonify(result=e)
@@ -95,7 +107,7 @@ def del_user():
 	try:
 		usr=request.form['nameofuser']
 		level1=g.cursor.execute('select level from user where uaername=(?)', [usr]).fetchall()
-		level2=g.cursor.execute('select level from user where uaername=(?)', [usrtemp]).fetchall()
+		level2=g.cursor.execute('select level from user where uaername=(?)', [USRTEMP]).fetchall()
 		if level1 > level2:
 			g.cursor.execute('delete from user where uaername=(?)', [usr])
 			g.db.commit()
